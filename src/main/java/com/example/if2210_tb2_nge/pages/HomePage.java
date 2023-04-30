5f23beebe356d56ced00bf8d14e6d5fce872f830
Ditra Amadia <60308058+ditramadia@users.noreply.github.com>
2023-05-01 02:23:19 +0700

    feat: add plugin menu and about menu (#3)

--- src/main/java/com/example/if2210_tb2_nge/pages/HomePage.java
+++ src/main/java/com/example/if2210_tb2_nge/pages/HomePage.java
@@ -32,20 +32,21 @@ public class HomePage {
         container = new VBox();
         container.setAlignment(Pos.TOP_CENTER);
 
-        //side bar
+        // side bar
         sideMenu = new VBox();
         button1 = new Button("Button 1");
         button2 = new Button("Button 2");
         button3 = new Button("Button 3");
         sideMenu.getChildren().addAll(button1,button2,button3);
         sideMenu.setStyle("-fx-background-color: #ADD8E6;");
-//        sideMenu.setAlignment(Pos.TOP_LEFT);
         sideMenu.setPrefWidth(200);
 
+        // clock
         clock = new Clock();
         VBox.setMargin(clock.getClockLabel(), new Insets(100,0,0,0));
         clock.setClock();
 
+        // author name and nim
         Label kel1 = new Label("Kelvin Rayhan A.       13521005");
         Label kel2 = new Label("Ditra R. Amadia         13521019");
         Label kel3 = new Label("Varraz Hazzandra A.   13521020");
@@ -57,6 +58,7 @@ public class HomePage {
         VBox.setMargin(kel1,new Insets(50, 10 ,0 ,10));
         kel.setFont(new Font(40));
 
+        // add component to layout
         container.getChildren().add(clock.getClockLabel());
         container.getChildren().add(kel);
         container.getChildren().addAll(kel1,kel2,kel3,kel4,kel5);
--- src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
+++ src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
@@ -9,22 +9,8 @@ import javafx.scene.text.Font;
 
 public class MenuPage {
     private VBox layout;
-    private Clock clock;
 
     public MenuPage(){
-        layout = new VBox();
-        layout.setAlignment(Pos.TOP_CENTER);
-
-        clock = new Clock();
-        VBox.setMargin(clock.getClockLabel(), new Insets(30,0,0,0));
-        clock.setClock();
-
-        Label kel = new Label("NTOT");
-        VBox.setMargin(kel,new Insets(100, 10 ,0 ,10));
-        kel.setFont(new Font(70));
-
-        layout.getChildren().add(kel);
-        layout.getChildren().add(clock.getClockLabel());
     }
 
     public VBox getlayout(){
