f64381b97a2f81b7af3e29b8b544c80ee0a35b8c
bernarduswillson <16521486@mahasiswa.itb.ac.id>
2023-05-01 02:05:57 +0700

    feat: add select function and can add multiple products

--- src/main/java/com/example/if2210_tb2_nge/HelloApplication.java
+++ src/main/java/com/example/if2210_tb2_nge/HelloApplication.java
@@ -4,6 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.embed.swing.SwingNode;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -12,6 +16,8 @@ import javafx.stage.PopupWindow;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
+
+import java.awt.*;
 import java.io.IOException;
 import javafx.animation.*;
 import javafx.util.*;
@@ -26,6 +32,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
+import java.util.List;
 
 
 public class HelloApplication extends Application {
@@ -111,34 +118,96 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public static void writeJsonSimpleDemo(String filename) throws Exception {
-        JSONObject sampleObject = new JSONObject();
-        sampleObject.put("name", "Stackabuser");
-        sampleObject.put("age", 35);
-
-        JSONArray messages = new JSONArray();
-        messages.add("Hey!");
-        messages.add("What's up?!");
-
-        sampleObject.put("messages", messages);
-
-        byte[] jsonBytes = sampleObject.toJSONString().getBytes();
-
-        Files.write(Paths.get(filename), jsonBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.SYNC);
-    }
-
     public static JSONObject readJsonFile(String filename) throws Exception {
         JSONParser parser = new JSONParser();
         Object obj = parser.parse(new FileReader(filename));
         JSONObject jsonObj = (JSONObject) obj;
-        String name = (String) jsonObj.get("name");
-        System.out.println("Name: " + name);
         return jsonObj;
+    }
+    public static void addProductToJsonDatabase(String filename, int id, String name, int price, int stock) throws Exception {
+        // Read existing JSON data from file into a JSONObject
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(filename));
+        JSONObject jsonObj = (JSONObject) obj;
+
+        // Create a new JSON object to represent the new product
+        JSONObject product = new JSONObject();
+        product.put("id", id);
+        product.put("name", name);
+        product.put("price", price);
+        product.put("stock", stock);
+
+        // Add the new product to the existing JSON data
+        JSONArray products = (JSONArray) jsonObj.get("products");
+        products.add(product);
+
+        // Write the updated JSON data back to the file
+        byte[] jsonBytes = jsonObj.toJSONString().getBytes();
+        Files.write(Paths.get(filename), jsonBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.SYNC);
+    }
+
+    // make a function like select * from products where stock is above x
+    public static JSONArray selectProductsWhereStockIsAbove(String filename, int stock) throws Exception {
+        // Read existing JSON data from file into a JSONObject
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(new FileReader(filename));
+        JSONObject jsonObj = (JSONObject) obj;
+
+        // Get the products array from the JSON object
+        JSONArray products = (JSONArray) jsonObj.get("products");
+
+        // Create a new JSON array to store the selected products
+        JSONArray selectedProducts = new JSONArray();
+
+        // Loop through the products array and select the products where stock is above x
+        for (Object productObj : products) {
+            JSONObject product = (JSONObject) productObj;
+            if ((long) product.get("stock") > stock) {
+                selectedProducts.add(product);
+            }
+        }
+
+        return selectedProducts;
     }
 
     public static void main(String[] args) throws Exception {
-        writeJsonSimpleDemo("example.json");
-        readJsonFile("example.json");
+        // Create a new JSON object to represent the initial database
+        JSONObject initialData = new JSONObject();
+        JSONArray products = new JSONArray();
+        initialData.put("products", products);
+
+        // Write the initial database to a file
+        byte[] jsonBytes = initialData.toJSONString().getBytes();
+        Files.write(Paths.get("products.json"), jsonBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.SYNC);
+
+        // Add some products to the database
+        addProductToJsonDatabase("products.json", 1, "kulkas", 100000, 10);
+        addProductToJsonDatabase("products.json", 2, "lemari", 10000, 5);
+
+        // Read the database from the file and print the contents
+        JSONObject jsonObj = readJsonFile("products.json");
+        JSONArray productsArray = (JSONArray) jsonObj.get("products");
+        for (Object productObj : productsArray) {
+            JSONObject product = (JSONObject) productObj;
+            System.out.println("id: " + product.get("id"));
+            System.out.println("name: " + product.get("name"));
+            System.out.println("price: " + product.get("price"));
+            System.out.println("stock: " + product.get("stock"));
+            System.out.println();
+        }
+
+        System.out.println("Select products where stock is above 7 and print the contents");
+        // Select products where stock is above 7 and print the contents
+        JSONArray selectedProducts = selectProductsWhereStockIsAbove("products.json", 7);
+        for (Object productObj : selectedProducts) {
+            JSONObject product = (JSONObject) productObj;
+            System.out.println("id: " + product.get("id"));
+            System.out.println("name: " + product.get("name"));
+            System.out.println("price: " + product.get("price"));
+            System.out.println("stock: " + product.get("stock"));
+            System.out.println();
+        }
+
         launch();
     }
 }
