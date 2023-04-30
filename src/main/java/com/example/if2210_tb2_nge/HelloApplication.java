8ead1e0db55fc752951744293eb04a539bc3672b
Kelvin <13521005@mahasiswa.itb.ac.id>
2023-05-01 01:43:15 +0700

    fix: newTab

--- src/main/java/com/example/if2210_tb2_nge/HelloApplication.java
+++ src/main/java/com/example/if2210_tb2_nge/HelloApplication.java
@@ -1,111 +1,111 @@
-package com.example.if2210_tb2_nge;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.embed.swing.SwingNode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.stage.PopupWindow;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.scene.control.*;
-import java.io.IOException;
-import javafx.animation.*;
-import javafx.util.*;
-import javafx.util.Duration;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
-import javafx.geometry.*;
-
-public class HelloApplication extends Application {
-    Button button;
-    Clock clock;
-    HomePage homePage;
-    MenuPage menu;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        stage.setTitle("JAVAAAAAA");
-        BorderPane root = new BorderPane();
-
-        MenuBar menuBar = new MenuBar();
-        Menu file = new Menu("File");        Menu settings = new Menu ("Settings");
-
-
-        menuBar.setUseSystemMenuBar(true);
-
-        menuBar.getMenus().addAll(file,settings);
-
-        MenuItem item1 = new MenuItem("Open");
-        MenuItem item2 = new MenuItem("Save");
-        MenuItem item3 = new MenuItem("Exit");
-
-        file.getItems().addAll(item1,item2,item3);
-        root.setTop(menuBar);
-
-        TabPane tabPane = new TabPane();
-        Tab tab1 = new Tab("Tab 1");
-        BorderPane layouttab1 = new BorderPane();
+//package com.example.if2210_tb2_nge;
 //
-        homePage = new HomePage();
-        menu = new MenuPage();
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Scene;
+//import javafx.embed.swing.SwingNode;
+//import javafx.scene.layout.BorderPane;
+//import javafx.scene.layout.StackPane;
+//import javafx.scene.layout.VBox;
+//import javafx.scene.text.Font;
+//import javafx.stage.PopupWindow;
+//import javafx.stage.Screen;
+//import javafx.stage.Stage;
+//import javafx.scene.control.*;
+//import java.io.IOException;
+//import javafx.animation.*;
+//import javafx.util.*;
+//import javafx.util.Duration;
+//import java.time.*;
+//import java.time.format.DateTimeFormatter;
+//import javafx.geometry.*;
+//
+//public class HelloApplication extends Application {
+//    Button button;
+//    Clock clock;
+//    HomePage homePage;
+//    MenuPage menu;
+//
+//    @Override
+//    public void start(Stage stage) throws IOException {
+//        stage.setTitle("JAVAAAAAA");
+//        BorderPane root = new BorderPane();
+//
+//        MenuBar menuBar = new MenuBar();
+//        Menu file = new Menu("File");        Menu settings = new Menu ("Settings");
 //
 //
-        tab1.setContent(layouttab1);
+//        menuBar.setUseSystemMenuBar(true);
 //
-        tab1.setClosable(false);
+//        menuBar.getMenus().addAll(file,settings);
 //
-        VBox sideMenu = new VBox();
-        Button button1 = new Button("Button 1");
-        Button button2 = new Button("Button 2");
-        Button button3 = new Button("Button 3");
-        sideMenu.getChildren().addAll(button1,button2,button3);
-        sideMenu.setStyle("-fx-background-color: #ADD8E6;");
-        sideMenu.setAlignment(Pos.TOP_LEFT);
-        sideMenu.setPrefWidth(200);
-        layouttab1.setLeft(sideMenu);
-        StackPane mulscreen = new StackPane();
-
-        mulscreen.getChildren().add(homePage.getlayout());
-        mulscreen.getChildren().add(menu.getlayout());
-        button1.setOnAction(e -> {
-            mulscreen.getChildren().get(0).setVisible(true);
-            mulscreen.getChildren().get(1).setVisible(false);
-        });
-        button2.setOnAction(e -> {
-            mulscreen.getChildren().get(1).setVisible(true);
-            mulscreen.getChildren().get(0).setVisible(false);
-        });
-        layouttab1.setCenter(mulscreen);
-        mulscreen.getChildren().get(1).setVisible(false);
+//        MenuItem item1 = new MenuItem("Open");
+//        MenuItem item2 = new MenuItem("Save");
+//        MenuItem item3 = new MenuItem("Exit");
+//
+//        file.getItems().addAll(item1,item2,item3);
+//        root.setTop(menuBar);
+//
+//        TabPane tabPane = new TabPane();
+//        Tab tab1 = new Tab("Tab 1");
+//        BorderPane layouttab1 = new BorderPane();
+////
+//        homePage = new HomePage();
+//        menu = new MenuPage();
+////
+////
+//        tab1.setContent(layouttab1);
+////
+//        tab1.setClosable(false);
+////
+//        VBox sideMenu = new VBox();
+//        Button button1 = new Button("Button 1");
+//        Button button2 = new Button("Button 2");
+//        Button button3 = new Button("Button 3");
+//        sideMenu.getChildren().addAll(button1,button2,button3);
+//        sideMenu.setStyle("-fx-background-color: #ADD8E6;");
+//        sideMenu.setAlignment(Pos.TOP_LEFT);
+//        sideMenu.setPrefWidth(200);
+//        layouttab1.setLeft(sideMenu);
+//        StackPane mulscreen = new StackPane();
+//
+//        mulscreen.getChildren().add(homePage.getlayout());
+//        mulscreen.getChildren().add(menu.getlayout());
+//        button1.setOnAction(e -> {
+//            mulscreen.getChildren().get(0).setVisible(true);
+//            mulscreen.getChildren().get(1).setVisible(false);
+//        });
+//        button2.setOnAction(e -> {
+//            mulscreen.getChildren().get(1).setVisible(true);
+//            mulscreen.getChildren().get(0).setVisible(false);
+//        });
+//        layouttab1.setCenter(mulscreen);
+//        mulscreen.getChildren().get(1).setVisible(false);
+////
+////
+//        tabPane.getTabs().add(tab1);
 //
 //
-        tabPane.getTabs().add(tab1);
-
-
-
-        Tab newTab = new Tab("+");
-        tabPane.getTabs().add(newTab);
-
-        newTab.setOnSelectionChanged(e-> {
-            Tab tab = new Tab("New Tab");
-            tabPane.getTabs().add(tabPane.getTabs().size()-1,tab);
-            tabPane.getSelectionModel().select(tabPane.getTabs().size()-2);
-            System.out.println(tabPane.getTabs().size()-1);
-        });
-
-        root.setCenter(tabPane);
-        Scene scene = new Scene(root, 300, 250);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch();
-    }
-}
-
+//
+//        Tab newTab = new Tab("+");
+//        tabPane.getTabs().add(newTab);
+//
+//        newTab.setOnSelectionChanged(e-> {
+//            Tab tab = new Tab("New Tab");
+//            tabPane.getTabs().add(tabPane.getTabs().size()-1,tab);
+//            tabPane.getSelectionModel().select(tabPane.getTabs().size()-2);
+//            System.out.println(tabPane.getTabs().size()-1);
+//        });
+//
+//        root.setCenter(tabPane);
+//        Scene scene = new Scene(root, 300, 250);
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        launch();
+//    }
+//}
+//
--- src/main/java/com/example/if2210_tb2_nge/HomePage.java
+++ src/main/java/com/example/if2210_tb2_nge/HomePage.java
@@ -3,31 +3,67 @@ package com.example.if2210_tb2_nge;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.control.*;
+import lombok.Getter;
+import lombok.Setter;
 
 public class HomePage {
-    private VBox layout;
+    @Getter
+    private Tab tab;
+    private VBox container;
+    @Getter
+    private VBox sideMenu;
+    @Getter
+    private Button button1;
+    @Getter
+    private Button button2;
+    @Getter
+    private Button button3;
+    private BorderPane layout;
     private Clock clock;
 
     public HomePage(){
-        layout = new VBox();
-        layout.setAlignment(Pos.TOP_CENTER);
+        // layout
+        tab = new Tab("Home");
+        layout = new BorderPane();
+        container = new VBox();
+        container.setAlignment(Pos.TOP_CENTER);
+
+        //side bar
+        sideMenu = new VBox();
+        button1 = new Button("Button 1");
+        button2 = new Button("Button 2");
+        button3 = new Button("Button 3");
+        sideMenu.getChildren().addAll(button1,button2,button3);
+        sideMenu.setStyle("-fx-background-color: #ADD8E6;");
+//        sideMenu.setAlignment(Pos.TOP_LEFT);
+        sideMenu.setPrefWidth(200);
 
         clock = new Clock();
-        VBox.setMargin(clock.getClockLabel(), new Insets(30,0,0,0));
+        VBox.setMargin(clock.getClockLabel(), new Insets(100,0,0,0));
         clock.setClock();
 
-        Label kel = new Label("NGE");
+        Label kel1 = new Label("Kelvin Rayhan A.       13521005");
+        Label kel2 = new Label("Ditra R. Amadia         13521019");
+        Label kel3 = new Label("Varraz Hazzandra A.   13521020");
+        Label kel4 = new Label("Bernardus Willson     13521021");
+        Label kel5 = new Label("Raditya Naufal A.     13521022");
+        Label kel6 = new Label("Kenny Benaya N,       13521023");
+        Label kel = new Label("Gangguan Jawa");
         VBox.setMargin(kel,new Insets(80, 10 ,0 ,10));
-        kel.setFont(new Font(70));
+        VBox.setMargin(kel1,new Insets(50, 10 ,0 ,10));
+        kel.setFont(new Font(40));
 
-        layout.getChildren().add(kel);
-        layout.getChildren().add(clock.getClockLabel());
+        container.getChildren().add(clock.getClockLabel());
+        container.getChildren().add(kel);
+        container.getChildren().addAll(kel1,kel2,kel3,kel4,kel5);
+        layout.setCenter(container);
+        layout.setLeft(sideMenu);
+        tab.setContent(layout);
     }
 
-    public VBox getlayout(){
-        return layout;
-    }
 }
