3cca113ae7d5075cf3b25f992c7269f97f1e06ff
kennypanjaitan_ <13521023@std.stei.itb.ac.id>
2023-05-02 01:28:54 +0700

    fix: Separate each datastore according to context data (DataStore.java)

--- src/main/java/com/example/if2210_tb2_nge/controller/DataStore.java
+++ src/main/java/com/example/if2210_tb2_nge/controller/DataStore.java
@@ -10,138 +10,122 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class DataStore {
-    public static void createDataStoreJSON() throws Exception {
-        JSONObject initialData = new JSONObject();
-        JSONArray dataStore = new JSONArray();
-        initialData.put("dataStore", dataStore);
 
-        // items database
-        JSONObject items = new JSONObject();
-        JSONArray itemsArray = new JSONArray();
-        items.put("items", itemsArray);
-        dataStore.add(items);
-
-        // customers database
-        JSONObject customers = new JSONObject();
-        JSONArray customersArray = new JSONArray();
-        customers.put("customers", customersArray);
-        dataStore.add(customers);
-
-        // transactions database
-        JSONObject transactions = new JSONObject();
-        JSONArray transactionsArray = new JSONArray();
-        transactions.put("transactions", transactionsArray);
-        dataStore.add(transactions);
-
-        byte[] jsonBytes = initialData.toJSONString().getBytes();
-        Files.write(Paths.get("src/main/java/com/example/if2210_tb2_nge/database/DataStore.json"), jsonBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
+    /**
+     * Write a JSONObject to a JSON file
+     * @param context   context of the data store (name of the json file)
+     * @param data      JSONObject of the data to be written
+     */
+    public static void writeFileJSON(String context, JSONObject data) throws Exception {
+        byte[] jsonBytes = data.toJSONString().getBytes();
+        Files.write(Paths.get("src/main/java/com/example/if2210_tb2_nge/database/" + context + ".json"), jsonBytes,
+                StandardOpenOption.CREATE, StandardOpenOption.WRITE,
                 StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.SYNC);
     }
 
-    public static JSONObject readDataStoreJSON() throws Exception {
+
+    /**
+     * Create a new empty JSON file for data store
+     * @param context   context of the data store (name of the json file)
+     */
+    public static void createDataStoreJSON(String context) throws Exception {
+        JSONObject initialData = new JSONObject();
+        JSONArray dataStore = new JSONArray();
+        initialData.put(context, dataStore);
+
+        writeFileJSON(context, initialData);
+    }
+
+    /**
+     * Initialize multiple empty data stores
+     */
+    public static void initDataStoreJSON() throws Exception {
+        createDataStoreJSON("Items");
+        createDataStoreJSON("Customers");
+        createDataStoreJSON("Transactions");
+    }
+
+    /**
+     * Read the data store JSON file
+     * @return  JSONObject of the data store
+     */
+    public static JSONObject readDataStoreJSON(String context) throws Exception {
         JSONParser jsonParser = new JSONParser();
-        FileReader reader = new FileReader("src/main/java/com/example/if2210_tb2_nge/database/DataStore.json");
+        FileReader reader = new FileReader("src/main/java/com/example/if2210_tb2_nge/database/" + context + ".json");
         return (JSONObject) jsonParser.parse(reader);
     }
 
-    public static JSONObject readSpecificJSON(String key) throws Exception {
-        JSONObject jsonObj = readDataStoreJSON();
-        JSONArray dataStore = (JSONArray) jsonObj.get("dataStore");
-        for (Object o : dataStore) {
-            JSONObject data = (JSONObject) o;
-            if (data.containsKey(key)) {
-                return data;
-            }
-        }
-        return null;
+    /**
+     * Add data to the data store JSON file
+     * @param context   context of the data store (name of the json file)
+     * @param data      JSONObject of the data to be added
+     */
+    public static void addDataStoreJSON(String context, JSONObject data) throws Exception {
+        JSONObject jsonObj = readDataStoreJSON(context);
+        JSONArray jsonArray = (JSONArray) jsonObj.get(context);
+        data.put("id", getLastId(context) + 1);
+        jsonArray.add(data);
+        writeFileJSON(context, jsonObj);
     }
 
-    public static void addDataStoreJSON(String key, JSONObject data) throws Exception {
-        JSONObject jsonObj = readDataStoreJSON();
-        JSONArray dataStore = (JSONArray) jsonObj.get("dataStore");
-        for (Object o : dataStore) {
+    /**
+     * Update an existing data in the data store JSON file
+     * @param context   context of the data store (name of the json file)
+     * @param data      JSONObject of the new data
+     * @param Id        id of the data to be updated
+     */
+    public static void updateDataStoreJSON(String context, JSONObject data, Integer Id) throws Exception {
+        JSONObject jsonObj = readDataStoreJSON(context);
+        JSONArray jsonArray = (JSONArray) jsonObj.get(context);
+        for (Object o : jsonArray) {
             JSONObject dataObject = (JSONObject) o;
-            if (dataObject.containsKey(key)) {
-                JSONArray array = (JSONArray) dataObject.get(key);
-                data.put("id", getLastId(key) + 1);
-                array.add(data);
+            Integer dataID = Integer.parseInt(dataObject.get("id").toString());
+            if (dataID == Id) {
+                data.put("id", Id);
+                jsonArray.remove(dataObject);
+                jsonArray.add(data);
                 break;
             }
         }
-        byte[] jsonBytes = jsonObj.toJSONString().getBytes();
-        Files.write(Paths.get("src/main/java/com/example/if2210_tb2_nge/database/DataStore.json"), jsonBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
-                StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.SYNC);
+        writeFileJSON(context, jsonObj);
     }
 
-    public static void updateDataStoreJSON(String key, JSONObject data, Integer Id) throws Exception {
-        JSONObject jsonObj = readDataStoreJSON();
-        JSONArray dataStore = (JSONArray) jsonObj.get("dataStore");
-        for (Object o : dataStore) {
+    /**
+     * Delete an existing data in the data store JSON file
+     * @param context   context of the data store (name of the json file)
+     * @param Id        id of the data to be deleted
+     */
+    public static void deleteDataStoreJSON(String context, Integer Id) throws Exception {
+        JSONObject jsonObj = readDataStoreJSON(context);
+        JSONArray jsonArray = (JSONArray) jsonObj.get(context);
+        for (Object o : jsonArray) {
             JSONObject dataObject = (JSONObject) o;
-            if (dataObject.containsKey(key)) {
-                JSONArray array = (JSONArray) dataObject.get(key);
-                for (Object o1 : array) {
-                    JSONObject data1 = (JSONObject) o1;
-                    Integer dataID = Integer.parseInt(data1.get("id").toString());
-                    if (dataID == Id) {
-                        data.put("id", Id);
-                        array.remove(data1);
-                        array.add(data);
-                        break;
-                    }
-                }
+            Integer dataID = Integer.parseInt(dataObject.get("id").toString());
+            if (dataID == Id) {
+                jsonArray.remove(dataObject);
                 break;
             }
         }
-        byte[] jsonBytes = jsonObj.toJSONString().getBytes();
-        Files.write(Paths.get("src/main/java/com/example/if2210_tb2_nge/database/DataStore.json"), jsonBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
-                StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.SYNC);
+        writeFileJSON(context, jsonObj);
     }
 
-    // delete data in database
-    public static void deleteDataStoreJSON(String key, Integer Id) throws Exception {
-        JSONObject jsonObj = readDataStoreJSON();
-        JSONArray dataStore = (JSONArray) jsonObj.get("dataStore");
-        for (Object o : dataStore) {
-            JSONObject dataObject = (JSONObject) o;
-            if (dataObject.containsKey(key)) {
-                JSONArray array = (JSONArray) dataObject.get(key);
-                for (Object o1 : array) {
-                    JSONObject data1 = (JSONObject) o1;
-                    Integer dataID = Integer.parseInt(data1.get("id").toString());
-                    if (dataID == Id) {
-                        array.remove(data1);
-                        break;
-                    }
-                }
-                break;
-            }
+    /**
+     * Get the last id of the data store
+     * @return last id of the data store
+     */
+    public static int getLastId(String context) throws Exception {
+        JSONObject jsonObj = readDataStoreJSON(context);
+        JSONArray jsonArray = (JSONArray) jsonObj.get(context);
+        try {
+            JSONObject lastData = (JSONObject) jsonArray.get(jsonArray.size() - 1);
+            return Integer.parseInt(lastData.get("id").toString());
+        } catch (Exception e) {
+            return 0;
         }
-        byte[] jsonBytes = jsonObj.toJSONString().getBytes();
-        Files.write(Paths.get("src/main/java/com/example/if2210_tb2_nge/database/DataStore.json"), jsonBytes, StandardOpenOption.CREATE, StandardOpenOption.WRITE,
-                StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.SYNC);
-    }
-
-    public static int getLastId(String key) throws Exception {
-        JSONObject jsonObj = readDataStoreJSON();
-        JSONArray dataStore = (JSONArray) jsonObj.get("dataStore");
-        for (Object o : dataStore) {
-            JSONObject data = (JSONObject) o;
-            if (data.containsKey(key)) {
-                JSONArray array = (JSONArray) data.get(key);
-                try {
-                    JSONObject lastData = (JSONObject) array.get(array.size() - 1);
-                    return Integer.parseInt(lastData.get("id").toString());
-                } catch (Exception e) {
-                    return 0;
-                }
-            }
-        }
-        return 0;
     }
 
     public static void main(String[] args) throws Exception {
-        createDataStoreJSON();
+        initDataStoreJSON();
 
         JSONObject item = new JSONObject();
         item.put("name", "Buku");
@@ -149,37 +133,37 @@ public class DataStore {
         item.put("stock", 10);
         item.put("category", "Buku");
         item.put("image", "www.pornhub.com");
-        addDataStoreJSON("items", item);
-        addDataStoreJSON("items", item);
+        addDataStoreJSON("Items", item);
+        addDataStoreJSON("Items", item);
 
         JSONObject customer = new JSONObject();
         customer.put("name", "Budi");
         customer.put("noTelp", "08123456789");
         customer.put("vip", false);
         customer.put("active", true);
-        addDataStoreJSON("customers", customer);
-        addDataStoreJSON("customers", customer);
+        addDataStoreJSON("Customers", customer);
+        addDataStoreJSON("Customers", customer);
 
         JSONObject transaction = new JSONObject();
         transaction.put("customerId", 1);
         transaction.put("totalPrice", 10000);
         transaction.put("date", "2021-04-01");
-        addDataStoreJSON("transactions", transaction);
+        addDataStoreJSON("Transactions", transaction);
 
-        //update items
+//        update items
         JSONObject itemUpdate = new JSONObject();
         itemUpdate.put("name", "Bukuuuu");
         itemUpdate.put("price", 10000);
         itemUpdate.put("stock", 10);
         itemUpdate.put("category", "Buku");
         itemUpdate.put("image", "www.pornhub.com");
-        updateDataStoreJSON("items", itemUpdate, 1);
+        updateDataStoreJSON("Items", itemUpdate, 1);
 
-        //delete customer
-        deleteDataStoreJSON("customers", 1);
+//        delete customer
+        deleteDataStoreJSON("Customers", 1);
 
-        JSONObject tes = readSpecificJSON("items");
-        JSONArray items = (JSONArray) tes.get("items");
+        JSONObject tes = readDataStoreJSON("Items");
+        JSONArray items = (JSONArray) tes.get("Items");
         for (Object o : items) {
             JSONObject data = (JSONObject) o;
             System.out.println(data.get("id"));
@@ -190,8 +174,8 @@ public class DataStore {
             System.out.println(data.get("image"));
         }
 
-        JSONObject tes1 = readSpecificJSON("customers");
-        JSONArray customers = (JSONArray) tes1.get("customers");
+        JSONObject tes1 = readDataStoreJSON("Customers");
+        JSONArray customers = (JSONArray) tes1.get("Customers");
         for (Object o : customers) {
             JSONObject data = (JSONObject) o;
             System.out.println(data.get("id"));
