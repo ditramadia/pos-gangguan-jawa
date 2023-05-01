c44fcd12d6f06ba0fcbbda7f7210633e4526a797
bernarduswillson <16521486@mahasiswa.itb.ac.id>
2023-05-01 17:30:13 +0700

    feat: add item detail page

---
+++ src/main/java/com/example/if2210_tb2_nge/pages/ItemDetailPage.java
@@ -0,0 +1,62 @@
+package com.example.if2210_tb2_nge.pages;
+
+import com.example.if2210_tb2_nge.controller.ItemController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import lombok.Getter;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class ItemDetailPage {
+    @Getter
+    private BorderPane pageContainer;
+    private VBox contentContainer;
+    private Label title;
+    private Label name;
+    private Label price;
+    private Label stock;
+    private Label buyPrice;
+    private Label category;
+
+    public ItemDetailPage() {
+        pageContainer = new BorderPane();
+        contentContainer = new VBox();
+        pageContainer.setCenter(contentContainer);
+
+        title = new Label("ITEM DETAILS");
+        name = new Label("Name: ");
+        price = new Label("Price: ");
+        stock = new Label("Stock: ");
+        buyPrice = new Label("Buy Price: ");
+        category = new Label("Category: ");
+
+        contentContainer.getChildren().add(title);
+        contentContainer.getChildren().add(name);
+        contentContainer.getChildren().add(price);
+        contentContainer.getChildren().add(stock);
+        contentContainer.getChildren().add(buyPrice);
+        contentContainer.getChildren().add(category);
+    }
+
+    public void loadData(int id) throws Exception {
+        // load data from database
+        // set data to labels
+
+        JSONObject jsonObj = ItemController.readItemsJSON("src/main/java/com/example/if2210_tb2_nge/database/Items.json");
+        JSONArray itemsArray = (JSONArray) jsonObj.get("items");
+        for (Object itemsObj : itemsArray) {
+            JSONObject product = (JSONObject) itemsObj;
+            if (Integer.parseInt(product.get("id").toString()) == id) {
+                name.setText("Name: " + product.get("name").toString());
+                price.setText("Price: " + product.get("price").toString());
+                stock.setText("Stock: " + product.get("stock").toString());
+                buyPrice.setText("Buy Price: " + product.get("buyPrice").toString());
+                category.setText("Category: " + product.get("category").toString());
+            }
+        }
+
+    }
+}
--- src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
+++ src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
@@ -3,12 +3,15 @@ package com.example.if2210_tb2_nge.pages;
 import com.example.if2210_tb2_nge.components.ItemCard;
 import com.example.if2210_tb2_nge.components.SearchBar;
 import com.example.if2210_tb2_nge.controller.ItemController;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.Label;
@@ -17,9 +20,11 @@ import lombok.Getter;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-public class MenuPage {
+public class MenuPage implements EventHandler<ActionEvent> {
     @Getter
     private Tab tab;
+    private StackPane mulscreens;
+    private ItemDetailPage itemDetailPage;
     private GridPane cardLayout;
     private BorderPane pageContainer;
     private VBox contentContainer;
@@ -35,12 +40,19 @@ public class MenuPage {
         // tab
         tab = new Tab("Inventory");
 
+        // pages container
+        mulscreens = new StackPane();
+        itemDetailPage = new ItemDetailPage();
+
         // page container
         pageContainer = new BorderPane();
 
         // content container
         contentContainer = new VBox();
         pageContainer.setCenter(contentContainer);
+        mulscreens.getChildren().add(pageContainer);
+        mulscreens.getChildren().add(itemDetailPage.getPageContainer());
+        mulscreens.getChildren().get(1).setVisible(false);
 
         // header
         header = new Label("INVENTORY");
@@ -75,6 +87,17 @@ public class MenuPage {
             JSONObject product = (JSONObject) itemsObj;
             Long id = (Long) product.get("id");
             ItemCard itemCard8 = new ItemCard(id.intValue());
+
+            itemCard8.getViewDetailBtn().setOnAction(e -> {
+                try {
+                    itemDetailPage.loadData(id.intValue());
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+                mulscreens.getChildren().get(0).setVisible(false);
+                mulscreens.getChildren().get(1).setVisible(true);
+            });
+
             GridPane.setMargin(itemCard8.getCardContainer(), new Insets(30));
             cardLayout.add(itemCard8.getCardContainer(), column++, row);
 
@@ -94,8 +117,16 @@ public class MenuPage {
         newItemBtn = new Button("New Item");
         BorderPane.setAlignment(newItemBtn, Pos.BOTTOM_RIGHT);
         pageContainer.setBottom(newItemBtn);
+        tab.setContent(mulscreens);
 
-        tab.setContent(pageContainer);
+    }
 
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+
+        System.out.println("View Detail Button Clicked");
+        mulscreens.getChildren().get(0).setVisible(false);
+        mulscreens.getChildren().get(1).setVisible(true);
     }
 }
