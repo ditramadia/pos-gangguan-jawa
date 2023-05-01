da14dcc2f2238495224b2fee4fac80beb58062ed
bernarduswillson <16521486@mahasiswa.itb.ac.id>
2023-05-02 03:26:56 +0700

    feat: add edit items example

--- src/main/java/com/example/if2210_tb2_nge/pages/ItemDetailPage.java
+++ src/main/java/com/example/if2210_tb2_nge/pages/ItemDetailPage.java
@@ -3,7 +3,9 @@ package com.example.if2210_tb2_nge.pages;
 import com.example.if2210_tb2_nge.controller.ItemController;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import lombok.Getter;
@@ -22,6 +24,14 @@ public class ItemDetailPage {
     private Label stock;
     private Label buyPrice;
     private Label category;
+    private Label line;
+    private Label stockEdit;
+    private Label priceEdit;
+    private TextField stockField;
+    private TextField priceField;
+
+    private Integer id;
+
 
     public ItemDetailPage() {
         pageContainer = new BorderPane();
@@ -34,6 +44,12 @@ public class ItemDetailPage {
         stock = new Label("Stock: ");
         buyPrice = new Label("Buy Price: ");
         category = new Label("Category: ");
+        line = new Label("--------------------------------------------------");
+        stockEdit = new Label("Edit Stock: ");
+        priceEdit = new Label("Edit Price: ");
+
+        stockField = new TextField();
+        priceField = new TextField();
 
         contentContainer.getChildren().add(title);
         contentContainer.getChildren().add(name);
@@ -41,11 +57,34 @@ public class ItemDetailPage {
         contentContainer.getChildren().add(stock);
         contentContainer.getChildren().add(buyPrice);
         contentContainer.getChildren().add(category);
+
+        contentContainer.getChildren().add(line);
+
+        contentContainer.getChildren().add(stockEdit);
+        contentContainer.getChildren().add(stockField);
+
+        contentContainer.getChildren().add(priceEdit);
+        contentContainer.getChildren().add(priceField);
+
+        Button updateStockButton = new Button("Update");
+        updateStockButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    updateData(id);
+                    // refresh page
+                    loadData(id);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        contentContainer.getChildren().add(updateStockButton);
     }
 
     public void loadData(int id) throws Exception {
-        // load data from database
-        // set data to labels
+        this.id = id;
 
         JSONObject jsonObj = ItemController.readItemsJSON("src/main/java/com/example/if2210_tb2_nge/database/Items.json");
         JSONArray itemsArray = (JSONArray) jsonObj.get("items");
@@ -60,5 +99,17 @@ public class ItemDetailPage {
             }
         }
 
+    }
+
+    public void updateData(int id) throws Exception {
+        JSONObject jsonObj = ItemController.readItemsJSON("src/main/java/com/example/if2210_tb2_nge/database/Items.json");
+        JSONArray itemsArray = (JSONArray) jsonObj.get("items");
+        for (Object itemsObj : itemsArray) {
+            JSONObject item = (JSONObject) itemsObj;
+            if (Integer.parseInt(item.get("id").toString()) == id) {
+                ItemController.updateItemsJSON(id, item.get("name").toString(), Integer.parseInt(priceField.getText()), Integer.parseInt(item.get("buyPrice").toString()), Integer.parseInt(stockField.getText()), item.get("category").toString(), item.get("image").toString()) ;
+                break;
+            }
+        }
     }
 }
