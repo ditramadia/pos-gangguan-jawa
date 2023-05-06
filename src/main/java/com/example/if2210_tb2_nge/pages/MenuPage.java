7777d29c8128b828bd44f9ac98397fc2a3189ff0
bernarduswillson <16521486@mahasiswa.itb.ac.id>
2023-05-06 22:14:21 +0700

    feat: connect transaction to database

--- src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
+++ src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
@@ -2,6 +2,8 @@ package com.example.if2210_tb2_nge.pages;
 
 import com.example.if2210_tb2_nge.components.MenuCard;
 import com.example.if2210_tb2_nge.components.SearchBar;
+import com.example.if2210_tb2_nge.entity.Items;
+import com.example.if2210_tb2_nge.repository.ItemsRepository;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -12,6 +14,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import lombok.Getter;
+
+import java.util.List;
 
 public class MenuPage {
     @Getter
@@ -38,13 +42,16 @@ public class MenuPage {
         searchBar = new SearchBar();
 
 
-        for (int i = 0; i < 10; i++){
-            MenuCard newMenuCard = new MenuCard();
-            GridPane.setMargin(newMenuCard.getCardContainer(), new Insets(30));
-            cardContainer.add(newMenuCard.getCardContainer(), column++, row);
-            if (column == 6){
-                row++;
+        List<Items> items = ItemsRepository.getItems();
+        for (Items item : items) {
+            Integer id = item.getId();
+            MenuCard card = new MenuCard(id);
+            GridPane.setMargin(card.getCardContainer(), new Insets(30));
+            cardContainer.add(card.getCardContainer(), column, row);
+            column++;
+            if (column == 6) {
                 column = 0;
+                row++;
             }
         }
 
