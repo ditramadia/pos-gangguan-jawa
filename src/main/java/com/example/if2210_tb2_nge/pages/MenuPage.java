a7e886dc92cc83465e389cbade0d2fc8a377090b
bernarduswillson <16521486@mahasiswa.itb.ac.id>
2023-05-08 04:46:49 +0700

    feat: menu page styling

--- src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
+++ src/main/java/com/example/if2210_tb2_nge/pages/MenuPage.java
@@ -1,5 +1,6 @@
 package com.example.if2210_tb2_nge.pages;
 
+import com.example.if2210_tb2_nge.components.ItemCard;
 import com.example.if2210_tb2_nge.components.MenuCard;
 import com.example.if2210_tb2_nge.components.SearchBar;
 import com.example.if2210_tb2_nge.controller.ItemController;
@@ -8,16 +9,16 @@ import com.example.if2210_tb2_nge.entity.Bill;
 import com.example.if2210_tb2_nge.entity.CartItem;
 import com.example.if2210_tb2_nge.entity.Items;
 import com.example.if2210_tb2_nge.repository.ItemsRepository;
+import javafx.animation.PauseTransition;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Tab;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 import lombok.Getter;
 
 import java.net.MalformedURLException;
@@ -34,7 +35,7 @@ public class MenuPage {
     private VBox contentContainer;
     private ScrollPane scrollPane;
     private GridPane cardContainer;
-    private Label title;
+    private Label header;
     private SearchBar searchBar;
     private Button cart;
     private int row = 1;
@@ -43,12 +44,15 @@ public class MenuPage {
     List<CartItem> cartItems;
     private CartPage cartPage;
 
-    public MenuPage() throws MalformedURLException {
+    public MenuPage() throws Exception {
         // Cart Items
         cartItems = new ArrayList<>();
 
         // Tab
         tab = new Tab("Menu");
+        tab.setStyle("-fx-background-radius: 10 10 0 0;");
+        TabPane tabPane = new TabPane();
+        tabPane.getTabs().add(tab);
 
         // Tab Container
         tabContainer = new StackPane();
@@ -71,38 +75,44 @@ public class MenuPage {
         contentContainer = new VBox();
         pageContainer.setCenter(contentContainer);
 
-        // Search bar
-        searchBar = new SearchBar();
-        contentContainer.getChildren().add(searchBar);
+        //
 
-        // Scroll Pane
+        // Title
+        header = new Label("MENU");
+        Font fontTitle = Font.loadFont("file:src/assets/Montserrat-Bold.ttf", 50);
+        header.setFont(fontTitle);
+        header.setStyle("-fx-text-fill: #478660;");
+        header.setAlignment(Pos.CENTER);
+        header.setPrefHeight(100);
+        header.setPrefWidth(500);
+        pageContainer.setAlignment(header, Pos.CENTER);
+        pageContainer.setTop(header);
+
+        // search bar
+        searchBar = new SearchBar();
+        contentContainer.getChildren().add(searchBar.getSearchBarContainer());
+        searchBar.getSearchField().textProperty().addListener((observable, oldValue, newValue) -> {
+            // Check if the text field has lost focus
+            PauseTransition pause = new PauseTransition(Duration.millis(10));
+            pause.setOnFinished(event -> {
+                try {
+                    this.updateCard();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            pause.play();
+        });
+
+        // scroll container
         scrollPane = new ScrollPane();
         contentContainer.getChildren().add(scrollPane);
 
-        // Card Container
+        // cards container
         cardContainer = new GridPane();
         scrollPane.setContent(cardContainer);
-
-        // Title
-        title = new Label("MENU");
-        title.setFont(new Font(30));
-        pageContainer.setTop(title);
-
-        // Menu cards
-        menuCards = new ArrayList<>();
-        List<Items> items = ItemsRepository.getItems();
-        for (Items item : items) {
-            Integer id = item.getId();
-            MenuCard card = new MenuCard(id);
-            menuCards.add(card);
-            GridPane.setMargin(card.getCardContainer(), new Insets(30));
-            cardContainer.add(card.getCardContainer(), column, row);
-            column++;
-            if (column == 6) {
-                column = 0;
-                row++;
-            }
-        }
+        scrollPane.setStyle("-fx-background: transparent; -fx-background-color: transparent;");
+        this.updateCard();
 
         // Cart button
         cart = new Button("Cart");
@@ -120,7 +130,47 @@ public class MenuPage {
             TransactionController.setBillInstance(new Bill(cartItems));
             cartPage.setCart();
         });
+        cart.setStyle("-fx-background-color: #478660; -fx-text-fill: white; -fx-font-size: 20px; -fx-font-weight: bold;");
+        cart.setPrefWidth(200);
+        cart.setPrefHeight(50);
+        BorderPane.setMargin(cart, new Insets(10, 85, 10, 0));
+        BorderPane.setAlignment(cart, Pos.BOTTOM_RIGHT);
         pageContainer.setBottom(cart);
     }
+
+    public void updateCard() throws Exception {
+        // create a new GridPane
+        GridPane newCardLayout = new GridPane();
+        menuCards = new ArrayList<>();
+
+        // get the items and iterate over them
+        List<Items> items = ItemsRepository.getItems();
+        for (Items item : items) {
+            // create a new ItemCard
+            Integer id = item.getId();
+            MenuCard itemCard = new MenuCard(id);
+            menuCards.add(itemCard);
+
+//            // check if the search field is empty or if the item name matches the search field
+            if (searchBar.getSearchField().getText().isEmpty() ||
+                    item.getName().toLowerCase().contains(searchBar.getSearchField().getText().toLowerCase())){
+                // add the card to the new card layout
+                GridPane.setMargin(itemCard.getCardContainer(), new Insets(30));
+                newCardLayout.add(itemCard.getCardContainer(), column++, row);
+                if (column == 6) {
+                    row++;
+                    column = 0;
+                }
+            }
+        }
+
+        // set the new card layout as the content of the scroll container
+        scrollPane.setContent(newCardLayout);
+
+        // reset the column and row counts
+        column = 0;
+        row = 1;
+    }
+
 
 }
