585f7726289637365c63fc6cf4b791abbf5b1d73
bernarduswillson <16521486@mahasiswa.itb.ac.id>
2023-05-07 21:58:33 +0700

    feat: done items

--- src/main/java/com/example/if2210_tb2_nge/components/ImageForm.java
+++ src/main/java/com/example/if2210_tb2_nge/components/ImageForm.java
@@ -6,6 +6,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import lombok.Getter;
+import lombok.Setter;
 
 public class ImageForm extends Control {
     @Getter
@@ -14,21 +15,26 @@ public class ImageForm extends Control {
     @Getter
     private Button editBtn;
     @Getter
+    @Setter
     private String imgUrl;
 
     public ImageForm(String imgPath) {
-        this.imgUrl = imgPath;
+        this.imgUrl = "https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/01/28/2114811954.jpg";
+        if (imgPath != null) {
+            this.imgUrl = imgPath;
+        }
         // image container
         imageContainer = new StackPane();
 
         // image view
-        image = new ImageView(new Image(imgPath));
-        image.setFitHeight(300);
-        image.setFitWidth(300);
+        image = new ImageView(new Image(imgUrl));
+        image.setFitHeight(200);
+        image.setFitWidth(200);
         imageContainer.getChildren().add(image);
 
         // edit button
         editBtn = new Button("Edit");
+        editBtn.setStyle("-fx-background-color: #83695A; -fx-text-fill: white;");
         imageContainer.getChildren().add(editBtn);
     }
 
